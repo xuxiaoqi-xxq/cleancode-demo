03923e4a81dd860d0f59d154ad45f36d3643d6a0
xuev6 <eva.x.q.xu@cargosmart.com>
2021-06-17 17:55:52 +0800

    refactor - move Order's field buyerUsername to User's field name

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -4,21 +4,30 @@ public class Order {
 
     private int orderNumber;
 
-    private String buyerName;
+    private User user;
+
     private String buyerPhoneNumber;
     private String buyerAddress;
 
     public Order(int orderNumber, String buyerName, String buyerPhoneNumber, String buyerAddress) {
         this.orderNumber = orderNumber;
 
-        this.buyerName = buyerName;
+        this.setBuyerName(buyerName);
         this.buyerPhoneNumber = buyerPhoneNumber;
         this.buyerAddress = buyerAddress;
     }
 
     public String confirm(){
         return String.format("Please confirm buyer information: buyer name is %s, " +
-                "buyer phone number is %s and buyer address is %s.", buyerName, buyerPhoneNumber, buyerAddress);
+                "buyer phone number is %s and buyer address is %s.", this.getBuyerName(), buyerPhoneNumber, buyerAddress);
+    }
+
+    public String getBuyerName() {
+        return this.user.getName();
+    }
+
+    public void setBuyerName(String buyerName) {
+        this.user = new User(buyerName);
     }
 }
 
---
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/User.java
@@ -0,0 +1,14 @@
+package com.tw.academy.basic.$5_data_clumps;
+
+public class User {
+
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
