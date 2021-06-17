79569645665292f13a7b4d149a34866bd6dfd98c
xuev6 <eva.x.q.xu@cargosmart.com>
2021-06-17 16:21:18 +0800

    refactor - auto refactor Parcel

--- src/test/java/com/tw/academy/basic/$5_data_clumps/ParcelTest.java
+++ src/test/java/com/tw/academy/basic/$5_data_clumps/ParcelTest.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ParcelTest {
     @Test
     public void should_confirm_receiver_information_with_sender_when_collect_parcel() {
-        Parcel parcel = new Parcel("Large", 12.5, "Tom", "13132323232", "Home",
-                "Jerry","13123333333", "Hole");
+        Parcel parcel = new Parcel("Large", 12.5,
+                new User("Tom", "13132323232", "Home"), new User("Jerry", "13123333333", "Hole"));
         String confirmReceiverMessage = parcel.confirmReceiver();
         String receiverExpect = "Please confirm receiver information: receiver name is Tom, receiver phone number is 13132323232 and receiver address is Home.\n";
 
