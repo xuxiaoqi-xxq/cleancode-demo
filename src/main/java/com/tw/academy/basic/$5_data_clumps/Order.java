599c04e8f8790db5af5ef83eaef551b7cb3c4fb2
xuev6 <eva.x.q.xu@cargosmart.com>
2021-06-17 18:24:06 +0800

    refactor - remove getter setter, init user

--- src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
+++ src/main/java/com/tw/academy/basic/$5_data_clumps/Order.java
@@ -8,39 +8,12 @@ public class Order {
 
     public Order(int orderNumber, String buyerName, String buyerPhoneNumber, String buyerAddress) {
         this.orderNumber = orderNumber;
-
-        this.setBuyerName(buyerName);
-        this.setBuyerPhoneNumber(buyerPhoneNumber);
-        this.setBuyerAddress(buyerAddress);
+        this.user = new User(buyerName, buyerPhoneNumber, buyerAddress);
     }
 
     public String confirm(){
         return String.format("Please confirm buyer information: buyer name is %s, " +
-                "buyer phone number is %s and buyer address is %s.", this.getBuyerName(), this.getBuyerPhoneNumber(), this.getBuyerAddress());
-    }
-
-    public String getBuyerName() {
-        return this.user.getName();
-    }
-
-    public void setBuyerName(String buyerName) {
-        this.user = new User(buyerName);
-    }
-
-    public String getBuyerPhoneNumber() {
-        return this.user.getPhoneNumber();
-    }
-
-    public void setBuyerPhoneNumber(String buyerPhoneNumber) {
-        this.user = new User(this.getBuyerName(), buyerPhoneNumber);
-    }
-
-    public String getBuyerAddress() {
-        return this.user.getAddress();
-    }
-
-    public void setBuyerAddress(String buyerAddress) {
-        this.user = new User(this.getBuyerName(), this.getBuyerPhoneNumber(), buyerAddress);
+                "buyer phone number is %s and buyer address is %s.", this.user.getName(), this.user.getPhoneNumber(), this.user.getAddress());
     }
 }
 
